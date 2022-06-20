c0dfa411a343eb70e00e5bf3a0d6760d0f364fce
Fulayfel <fawaztsa@gmail.com>
2022-06-20 18:56:05 +0000

    more gitpod config 🟠

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,15 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		vite: {
+			server: {
+				hmr: {
+					clientPort: process.env.HMR_HOST ? 443 : 24678,
+					host: process.env.HMR_HOST ? process.env.HMR_HOST.substring("https://".length) : "localhost"
+				}
+			}
+		}
 	}
 };
 
