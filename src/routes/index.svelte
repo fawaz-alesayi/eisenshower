05103560c73491d3250a0d0524a838938aa0b787
Fulayfel <fawaztsa@gmail.com>
2022-06-20 20:07:38 +0000

    add fonts and global css reset 🧼

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import "@fontsource/ibm-plex-sans"
+</script>
+
+<h1>Eisenshower</h1>
+
+<style lang="scss">
+    h1 { font-family: "IBM Plex Mono", monospace; }
+</style>
