6ce2a8c6089c50aea84a21db3bfd4c65fa592966
Fulayfel <fawaztsa@gmail.com>
2022-06-23 12:01:08 +0000

    fix store 👏

--- src/stores/todoStore.ts
+++ src/stores/todoStore.ts
@@ -7,16 +7,16 @@ const KEY_NAME = 'eisenshower_todos';
 function createTodoStore() {
 	// Set the stored value or a sane default.
 	const value = localStorage.getItem(KEY_NAME);
-	const todos = JSON.parse(value);
-	const store = writable<string[] | boolean>(browser && todos);
 
-	const { subscribe } = derived(store, ($store) => {
-		if ($store) return JSON.parse($store);
-		return null;
-	});
+	let todos = null;
+	if (value) {
+	todos = JSON.parse(value);
+	}
+
+	const store = writable<string[]>(browser && todos);
 
 	return {
-		subscribe,
+		subscribe: store.subscribe,
 		add: (item: string) =>
 			store.update((todos) => {
 				if (todos) return [...JSON.parse(todos), item];
