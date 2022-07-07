ae22024e1cbf3806a4d3e28fc19467c72037d00a
Fawaz Alesayi <fawaztsa@gmail.com>
2022-07-07 10:45:12 +0300

    use ngrok to access website from remote ips 📶

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,32 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { resolve } from 'path'
+import { resolve } from 'path';
 
+async function getHostUrl() {
+	if (!process.env.GITPOD_WORKSPACE_ID) {
+        // local with ngrok
+		let data = await fetch('http://127.0.0.1:4040/api/tunnels');
+		let json = await data.json();
+		return json.tunnels[0].public_url;
+	} else {
+        // gitpod
+		return process.env.HMR_HOST;
+	}
+}
 
 /** @type {import('vite').UserConfig} */
 const config = {
-        plugins: [sveltekit()],
-        server: {
-            hmr: {
-                clientPort: process.env.HMR_HOST ? 443 : 3000,
-                host: process.env.HMR_HOST ? process.env.HMR_HOST.substring("https://".length) : "localhost"
-            }
-        },
-        resolve: {
-            alias: {
-                $lib: resolve('./src/lib')
-            }
-        }
+	plugins: [sveltekit()],
+	server: {
+		hmr: {
+			clientPort: process.env.HMR_HOST ? 443 : 3000,
+			host: process.env.HMR_HOST ? (await getHostUrl()).match(/^(https:\/\/)?(.*)$/)[2] : 'localhost'
+		}
+	},
+	resolve: {
+		alias: {
+			$lib: resolve('./src/lib')
+		}
+	}
 };
 
 export default config;
